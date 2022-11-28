60c19d4c0207b0516bb1c57626ea2e53f1f7860a
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-11-28 23:14:25 +0200

    create Book class

---
+++ src/Book.java
@@ -0,0 +1,19 @@
+
+public class Book {
+    public Book(String id, String author, String title, String genre, String price, String publishDate, String description) {
+        this.id = id ;
+        this.author = author;
+        this.title = title;
+        this.genre = genre;
+        this.price = price;
+        this.publishDate = publishDate;
+        this.description = description;
+    }
+    String id;
+    String author;
+    String title;
+    String genre;
+    String price;
+    String publishDate;
+    String description;
+}
